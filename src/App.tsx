30c1f76e5f900b163de7cba09b49ec98379480e7
o8n <mywaymasahiro5862sook@gmail.com>
2024-07-13 13:32:16 +0900

    [add] counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, ChangeEvent } from 'react';
+// import './App.css';
+import { Container, TextField, Typography, Box } from '@mui/material';
 
-function App() {
+
+const App: React.FC = () => {
+  const [text, setText] = useState<string>('');
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h1>文字列カウンター</h1>
+    //     <textarea
+    //       value={text}
+    //       onChange={handleChange}
+    //       placeholder="文字列を記入してください"
+    //       rows={50}
+    //       cols={50}
+    //     />
+    //     <p>文字数: {text.length}</p>
+    //   </header>
+    // </div>
+    <Container maxWidth="sm">
+      <Box sx={{my:4, textAlign: 'center'}}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        文字数カウンター
+      </Typography>
+        <TextField
+          label="文字を入力してください"
+          multiline
+          rows={20}
+          variant="outlined"
+          fullWidth
+          value={text}
+          onChange={handleChange}
+        />
+        <Typography variant="h6" component="p" sx={{ mt: 2 }}>
+          文字数: {text.length}
+        </Typography>
+      </Box>
+    </Container>
   );
 }
 
